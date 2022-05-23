413ebe806dffadf9652b4b6c9593f8c31c8f10ab
Jay-ar Dela Rama <jayardelarama27@gmail.com>
2022-05-23 21:53:03 +0800

    handles math operation

--- calculator.js
+++ calculator.js
@@ -6,21 +6,53 @@ const screen = document.querySelector('.screen');
 
 function buttonClick(value) {
     if (isNaN(value)){
+        //not number
         handleSymbol(value);
     }else {
+        // number
         handleNumber(value);
     }
-}
-
-function handleSymbol(symbol) {}
-
-function handleNumber(numberString) {
-    if(buffer === "0"){
-        buffer = numberString;
-    }else {
-        buffer += numberString;
-    }
     screen.innerText = buffer;
+}
+    // handles the symbols
+function handleSymbol(symbol) {
+    if(symbol === 'C'){
+        buffer = '0';
+        runningTotal = 0;
+    }
+}
+    // handles the numbers
+function handleNumber(numberString) {
+    // choosing between different operators
+    switch(value) {
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+        break;
+        case '+':
+        case '-':
+        case '&times':
+        case '&divide':
+            handleMath(symbol);
+        break; 
+    }
+}
+function handleMath() {
+    if(buffer === '0'){
+        // do nothing return statement
+        return
+    }
+    const intBuffer = parseInt(buffer);
+
+    if (runningTotal === 0){
+        // output buffer current values
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer)
+    }
+    previousOperator = symbol;
+
+    buffer = '0';
 }
 
 function init() {
